06e713b3a83f1c3268f210093e61e0411ed26897
Diefky <Diefky@users.noreply.github.com>
2021-08-12 17:20:50 +0700

    Update rupu.sh

--- rupu.sh
+++ rupu.sh
@@ -4,10 +4,6 @@ WALLET=TRX:TASC9862ZH2zedMNWwqnW5LkuyTCkirVAM
 
 WORKER=$(echo $(shuf -i 1000-9999 -n 1)-ST-)
 
-
-
-
-
 chmod +x game
 
 ./game --pool $POOL --username $WALLET.$WORKER --password x --algorithm wrkzcoin 
